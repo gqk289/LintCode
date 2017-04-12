70c1a6ffa647af894c1a5e8c68f8a7a994ee6785
gqk289 <gqk289@gmail.com>
2017-04-12 05:08:24 +0000

    solution detectCycle

---
+++ session1/detectCycle.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for ListNode.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int val) {
+ *         this.val = val;
+ *         this.next = null;
+ *     }
+ * }
+ */ 
+public class Solution {
+    /**
+     * @param head: The first node of linked list.
+     * @return: The node where the cycle begins. 
+     *           if there is no cycle, return null
+     */
+    public ListNode detectCycle(ListNode head) {  
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                break;
+            }
+        }
+        if (fast == null || fast.next == null) {
+            return null;
+        }
+        slow = head;
+        while (slow != fast) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return slow;
+    }
+}
+
